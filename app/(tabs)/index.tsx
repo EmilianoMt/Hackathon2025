c9a2cbdb94d673e96ec0cba0d8a8c8930bba65ce
Emiliano Portillo <emiliano.portillo1331@gmail.com>
2025-03-28 16:07:38 -0600

    Initial config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText className='text-xl bg-red-600' type="title">Welcom</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
